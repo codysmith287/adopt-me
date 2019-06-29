58b95b2099f63f15043d504e3d8abfcf544a0146
Cody Smith <cms287@gmail.com>
2019-06-28 21:06:35 -0700

    Created SearchParams component w/ react hooks, added eslint rules for react hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams.js";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "dog",
-      breed: "labrator"
-    }),
-    React.createElement(Pet, {
-      name: "Ginger",
-      animal: "dog",
-      breed: "weiner-dog"
-    }),
-    React.createElement(Pet, { name: "Sam", animal: "Bird", breed: "Tocan" })
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
