1f33e9652f9782c306a9f1c44153fd8ddc2f53d5
Cody Smith <cms287@gmail.com>
2019-07-02 12:26:48 -0700

    add Details route, add custom Babel config, add Carousel

---
+++ src/Carousel.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    active: 0
+  };
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick = e => {
+    // the '+' makes e.target.dataset.index a number
+    // using an arrow function does not create a new context when invoked and therefore the correct context of 'this' is found lexically.
+    this.setState({
+      active: +e.target.dataset.index
+    });
+  };
+
+  render() {
+    const { photos, active } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[active]} alt={`animal - ${active}`} />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => (
+            // not best practice for accessibility
+            // eslint-disable-next-line
+            <img
+              key={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              src={photo}
+              className={index === active ? "active" : ""}
+              alt="animal thumbnail"
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
